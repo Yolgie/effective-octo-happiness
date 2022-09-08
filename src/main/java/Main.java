e814740ae330f1d3638ff1b0ad8570635b067378
Matthias Holzinger <yolgie@cnoize.at>
2022-09-09 00:46:48 +0200

    auto 2022-09-08T22:46:48.966731Z

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i=0;i<=100;i++) {
+        for (int i = 0; i <= 100; i++) {
             System.out.println(fizzBuzz(i));
         }
     }
 
     public static String fizzBuzz(int target) {
-        if(target%3==0 && target%5==0) return "FizzBuzz";
-        else
-        if(target%5==0) return "Buzz";
-        else
-        if(target%3==0) return "Fizz";
-        else
-            return Integer.toString(target);
+        if (target % 3 == 0 && target % 5 == 0) return "FizzBuzz";
+        else if (target % 5 == 0) return "Buzz";
+        else if (target % 3 == 0) return "Fizz";
+        else return Integer.toString(target);
     }
 }
