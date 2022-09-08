de74c2c25871ea4351c6358e3690d08470ca8acc
Matthias Holzinger <matthias.holzinger@cloudflight.io>
2022-09-09 00:28:26 +0200

    gradle config for autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,24 @@
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.ResetCommand.ResetType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.time.Instant
+import java.time.format.DateTimeFormatter
 
 plugins {
     kotlin("jvm") version "1.7.10"
     application
 }
 
-group = "io.cloudflight.kata.refuktoring"
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.eclipse.jgit:org.eclipse.jgit:6.2.0.202206071550-r")
+    }
+}
+
+group = "io.cloudflight.kata.refucktoring"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -26,4 +39,40 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.withType<Test> {
+    addTestListener(object : TestListener {
+        override fun beforeSuite(suite: TestDescriptor) {}
+        override fun beforeTest(testDescriptor: TestDescriptor) {}
+        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {}
+        override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+            if (suite.name == "Gradle Test Run :test" &&
+                result.resultType == TestResult.ResultType.SUCCESS
+            ) {
+                commitWithTimestamp()
+            }
+        }
+    })
+}
+
+tasks.register("commit") {
+    commitWithTimestamp()
+}
+
+fun commitWithTimestamp() {
+    val git = Git.open(layout.projectDirectory.asFile)
+    val message = "auto " + DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+
+    git.add().addFilepattern(".").call()
+    git.commit().setMessage(message).call()
+
+    println("Committed with \"$message\"")
+}
+
+tasks.register("reset") {
+    doLast {
+        val git = Git.open(layout.projectDirectory.asFile)
+        git.reset().setMode(ResetType.HARD).call()
+    }
 }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -11,38 +11,38 @@ internal class MainTest {
 
     @Test
     fun `java returns 2 for 2`() {
-        assertEquals("2", fizzbuzz(2))
+        assertEquals("2", Main.fizzBuzz(2))
     }
 
     @Test
     fun `java returns Fizz for 3`() {
-        assertEquals("Fizz", fizzbuzz(3))
+        assertEquals("Fizz", Main.fizzBuzz(3))
     }
 
     @Test
     fun `java returns Fizz for 6`() {
-        assertEquals("Fizz", fizzbuzz(6))
+        assertEquals("Fizz", Main.fizzBuzz(6))
     }
 
     @Test
     fun `java returns Buzz for 5`() {
-        assertEquals("Buzz", fizzbuzz(5))
+        assertEquals("Buzz", Main.fizzBuzz(5))
     }
 
     @Test
     fun `java returns Buzz for 10`() {
-        assertEquals("Buzz", fizzbuzz(10))
+        assertEquals("Buzz", Main.fizzBuzz(10))
     }
 
     @Test
     fun `java returns FizzBuzz for 15`() {
-        assertEquals("FizzBuzz", fizzbuzz(15))
+        assertEquals("FizzBuzz", Main.fizzBuzz(15))
     }
 
     @Test
     fun `java test all`() {
         @Suppress("MaxLineLength")
         val fullSolution = arrayOf("1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz","16","17","Fizz","19","Buzz","Fizz","22","23","Fizz","Buzz","26","Fizz","28","29","FizzBuzz","31","32","Fizz","34","Buzz","Fizz","37","38","Fizz","Buzz","41","Fizz","43","44","FizzBuzz","46","47","Fizz","49","Buzz","Fizz","52","53","Fizz","Buzz","56","Fizz","58","59","FizzBuzz","61","62","Fizz","64","Buzz","Fizz","67","68","Fizz","Buzz","71","Fizz","73","74","FizzBuzz","76","77","Fizz","79","Buzz","Fizz","82","83","Fizz","Buzz","86","Fizz","88","89","FizzBuzz","91","92","Fizz","94","Buzz","Fizz","97","98","Fizz","Buzz")
-        fullSolution.forEachIndexed { index, expected -> assertEquals(expected, fizzbuzz(index+1)) }
+        fullSolution.forEachIndexed { index, expected -> assertEquals(expected, Main.fizzBuzz(index+1)) }
     }
 }
