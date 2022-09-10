feb56ecd707e27d9d617c0a6885bf8e82a547f0b
Matthias Holzinger <yolgie@cnoize.at>
2022-09-10 10:03:52 +0200

    woa!! this one was really HARD!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i <= 100; i++) {
-            System.out.println(fizzBuzz(i));
+            System.out.println(fb(i));
         }
     }
 
-    public static String fizzBuzz(int target) {
+    // bf does takes a number and sometimes returns a word
+    public static String fb(int target) {
+        // todo fix me ? have tried before, did not work
+
         if (target % 3 == 0 && target % 5 == 0) return "FizzBuzz";
         else if (target % 5 == 0) return "Buzz";
         else if (target % 3 == 0) return "Fizz";
